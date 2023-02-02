6af22b1e8d57cba91b78ba1d5caaa60c21b5bc28
todoni <sohan@student.42seoul.kr>
2023-02-02 17:49:55 +0900

    Delete: unnecessary lines

--- srcs/requirements/mariadb/run.sh
+++ srcs/requirements/mariadb/run.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
-for i in /scripts/pre-init.d/*sh
-do
-	if [ -e "${i}" ]; then
-		echo "[i] pre-init.d - processing $i"
-		. "${i}"
-	fi
-done
-
 if [ -d "/run/mysqld" ]; then
-	echo "[i] mysqld already present, skipping creation"
+	echo "mysqld already present, skipping creation"
 	chown -R mysql:mysql /run/mysqld
 else
-	echo "[i] mysqld not found, creating...."
+	echo "mysqld not found, creating...."
 	mkdir -p /run/mysqld
 	chown -R mysql:mysql /run/mysqld
 fi
@@ -26,15 +18,6 @@ else
 	chown -R mysql:mysql /var/lib/mysql
 
 	mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
-
-	if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
-		MYSQL_ROOT_PASSWORD=`pwgen 16 1`
-		echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
-	fi
-
-	MYSQL_DATABASE=${MYSQL_DATABASE:-""}
-	MYSQL_USER=${MYSQL_USER:-""}
-	MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
 
 	tfile=`mktemp`
 	if [ ! -f "$tfile" ]; then
@@ -50,14 +33,13 @@ SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
 DROP DATABASE IF EXISTS test ;
 FLUSH PRIVILEGES ;
 EOF
-	echo "database name: $MYSQL_DATABASE"
 	if [ "$MYSQL_DATABASE" != "" ]; then
-	    echo "[i] Creating database: $MYSQL_DATABASE"
+	    echo "Creating database: $MYSQL_DATABASE"
 		if [ "$MYSQL_CHARSET" != "" ] && [ "$MYSQL_COLLATION" != "" ]; then
-			echo "[i] with character set [$MYSQL_CHARSET] and collation [$MYSQL_COLLATION]"
+			echo "with character set [$MYSQL_CHARSET] and collation [$MYSQL_COLLATION]"
 			echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE CHARACTER SET $MYSQL_CHARSET COLLATE $MYSQL_COLLATION;" >> $tfile
 		else
-			echo "[i] with character set: 'utf8' and collation: 'utf8_general_ci'"
+			echo "with character set: 'utf8' and collation: 'utf8_general_ci'"
 			echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
 		fi
 
@@ -71,29 +53,11 @@ EOF
 	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tfile
 	rm -f $tfile
 
-	for f in /docker-entrypoint-initdb.d/*; do
-		case "$f" in
-			*.sql)    echo "$0: running $f"; /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < "$f"; echo ;;
-			*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < "$f"; echo ;;
-			*)        echo "$0: ignoring or entrypoint initdb empty $f" ;;
-		esac
-		echo
-	done
-
 	echo
 	echo 'MySQL init process done. Ready for start up.'
 	echo
 
 	echo "exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0" "$@"
 fi
-
-# execute any pre-exec scripts
-for i in /scripts/pre-exec.d/*sh
-do
-	if [ -e "${i}" ]; then
-		echo "[i] pre-exec.d - processing $i"
-		. ${i}
-	fi
-done
 
 exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
