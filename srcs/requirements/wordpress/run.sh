5deb7efd3040fd015c5fd2fa74132c7e106230ab
todoni <sohan@student.42seoul.kr>
2023-02-04 13:54:01 +0900

    Fix: locate www.conf manually that wordpress container succesfully restart whenever unexpectedly restarts

--- srcs/requirements/wordpress/run.sh
+++ srcs/requirements/wordpress/run.sh
@@ -9,6 +9,4 @@ if [ ! -f "/var/www/html/wordpress/index.php" ]; then
     wp-cli user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_USER_PASSWORD
 fi
 
-sed -i 's|^listen = 127.0.0.1:9000$|listen = 9000|g' /etc/php8/php-fpm.d/www.conf
-
 exec php-fpm8 -F -R
